811c4530eefc76d848a83065d9c3b4f20eb03b9a
Maxime ORGANI <morgani@e1r11p9.42.fr>
2019-01-23 16:45:13 +0100

    chgmt max

--- ft_fill_prt_spc.c
+++ ft_fill_prt_spc.c
@@ -6,7 +6,7 @@
 /*   By: morgani <morgani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 15:16:03 by morgani           #+#    #+#             */
-/*   Updated: 2019/01/22 09:49:19 by morgani          ###   ########.fr       */
+/*   Updated: 2019/01/23 16:11:57 by morgani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,13 +50,25 @@
 // 	}
 // }
 
+static int	ft_chck2(t_conv *c)
+{
+	return (c->tp == 'd') ? 1 : 0;
+}
+
+static int	ft_chck1(t_conv *c)
+{
+	return ((c->tp == 'c' || c->tp == 's') ? 1 : 0);
+}
+
 void		ft_fill_prt_spc(t_conv *c)
 {
 	c->prt_spc = 0;
-	if ((c->wdth > c->str_l && c->prc_sz > c->str_l) || (c->wdth > c->str_l && !c->prc))
-		c->prt_spc = c->wdth - c->str_l;
-	else if (c->prc && c->wdth > c->prc_sz)
+	if ((ft_chck1(c) && c->wdth > c->str_l && c->prc_sz > c->str_l) || (c->wdth > c->str_l && !c->prc) || (ft_chck1(c) && c->wdth > c->str_l))
+		c->prt_spc = ft_chck1(c) ?  c->wdth - c->str_l : c->wdth - c->str_l - c->flg_tp.pls;
+	else if (ft_chck2(c) && c->prc && c->wdth > c->prc_sz)
 		c->prt_spc = c->wdth - c->prc_sz;
+	else if (!ft_chck1(c) && c->prc && c->prc_sz > c->str_l)
+		c->prt_spc = c->prc_sz - c->str_l;
 	else if (c->wdth && c->prc && c->prc_sz == 0)
 		c->prt_spc = c->wdth;
 }
--- ft_print_tp_d.c
+++ ft_print_tp_d.c
@@ -6,7 +6,7 @@
 /*   By: morgani <morgani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 10:47:50 by morgani           #+#    #+#             */
-/*   Updated: 2019/01/22 09:54:20 by morgani          ###   ########.fr       */
+/*   Updated: 2019/01/23 15:40:13 by morgani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_print_d(t_conv *c, int i)
 		ft_prt_spc(c);
 	if (i == 1)
 		ft_print_flg(c);
-	if ((c->prc && c->prc_sz != 0) || (c->tp == 'd' && !c->prc))
+	if ((c->prc && c->prc_sz != 0) || (c->tp == 'd'))
 		c->sn ? ft_putbuff(c, ++c->str) : ft_putbuff(c, c->str);
 	if (c->flg_tp.mns)
 		ft_prt_spc(c);
@@ -28,6 +28,14 @@ void	ft_print_d(t_conv *c, int i)
 
 void	ft_print_tp_d(t_conv *c)
 {
+	if (c->sn)
+	{
+		c->flg_tp.spc = 0;
+		if (c->flg_tp.pls || c->prc_sz > c->str_l)
+			c->prt_spc++;
+	}
+	while (c->prc_sz-- > c->str_l)
+		ft_add_to_buff(c, "0");
 	if ((c->wdth > c->prc_sz) && c->prc_sz > c->str_l)
 		while (c->wdth-- > c->prc_sz + c->sn)
 			ft_add_to_buff(c, " ");
