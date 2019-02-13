7c76bf5a10cb309339c9eaa448372bbd777c4578
Valentin LECOQ-VALLON <vlecoq-v@e1r8p8.42.fr>
2019-02-13 20:11:11 +0100

    c'est la merde

--- ft_add_to_buff.c
+++ ft_add_to_buff.c
@@ -6,7 +6,7 @@
 /*   By: vlecoq-v <vlecoq-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 13:27:53 by morgani           #+#    #+#             */
-/*   Updated: 2019/02/13 15:13:43 by vlecoq-v         ###   ########.fr       */
+/*   Updated: 2019/02/13 20:10:59 by vlecoq-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int		ft_add_to_buff(t_conv *c, char *s)
 {
+	//prout
 	ft_strncpy(c->buff + c->ind++, s, 1);
 	if (c->ind == BUFF_SZ)
 		ft_reset_buff(c);
