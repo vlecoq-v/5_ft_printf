540943647608b6eac45515080e3e7e52c2643af9
Valentin LECOQ-VALLON <vlecoq-v@e1r8p8.42.fr>
2019-02-13 19:43:53 +0100

    asdf

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: vlecoq-v <vlecoq-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 16:13:12 by morgani           #+#    #+#             */
-/*   Updated: 2019/02/13 19:35:34 by vlecoq-v         ###   ########.fr       */
+/*   Updated: 2019/02/13 19:42:18 by vlecoq-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 void	ft_print_str(t_conv *c)
 {
-	printf("\nFT_PRINT_STR || CE QUI SERA PRINT  %c: \n\n", c->tp);
+	// printf("\nFT_PRINT_STR || CE QUI SERA PRINT  %c: \n\n", c->tp);
 	ft_prt_strct(c);
 	if (c->tp == 'c' || TP_MC)
 		ft_print_tp_c(c);
-	if ((c->tp == 's' && c->prc_sz != 0) || c->tp == '%' || c->tp == '!') // PROBLEM HIER SOIR BIERE
-	{
-		printf("caca print str\n");
+	if ((c->tp == 's') || c->tp == '%' || c->tp == '!') // PROBLEM HIER SOIR BIERE condition pou s c>prc_sz != 0
 		ft_print_tp_s(c);
-	}
 	if (c->tp == 'd' || c->tp == 'u' || c->tp == 'x'
 		|| c->tp == 'X' || c->tp == 'o' || c->tp == 'p' || c->tp == 'b' || c->tp == 'f')
 		ft_print_tp_d(c);
